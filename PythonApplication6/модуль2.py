7d5a9160b19d73712419a8570b4fd1b591c99fe8
qwer12211 <145342155+qwer12211@users.noreply.github.com>
2023-12-12 21:31:21 +0300

    Update модуль2.py

--- PythonApplication6/модуль2.py
+++ PythonApplication6/модуль2.py
@@ -1,5 +1,5 @@
 import random
-import s
+import bcrypt
 
 class User:
     def __init__(self, store):
@@ -8,7 +8,7 @@ class User:
 
     def validate_credentials(self, login, password):
         if not login.strip() or not password.strip():
-            print("Поле логинв и пароля не могут быть пустыми. Попробуйте снова.\n")
+            print("РџРѕР»Рµ Р»РѕРіРёРЅРІ Рё РїР°СЂРѕР»СЏ РЅРµ РјРѕРіСѓС‚ Р±С‹С‚СЊ РїСѓСЃС‚С‹РјРё. РџРѕРїСЂРѕР±СѓР№С‚Рµ СЃРЅРѕРІР°.\n")
             return False
         return True
 
@@ -17,25 +17,25 @@ class User:
             return False
 
         self.store.cursor.execute('SELECT * FROM users WHERE login = ?', (login,))
-        existing_user = self.store.cursor.fetchone()
+        sush_user = self.store.cursor.fetchone()
 
-        if existing_user and bcrypt.checkpw(password.encode('utf-8'), existing_user[2]):
-            self.authenticated_user = existing_user
+        if sush_user and bcrypt.checkpw(password.encode('utf-8'), sush_user[2]):
+            self.authenticated_user = sush_user
             return True
         else:
-            print("Неправильный логин или пароль.\n")
+            print("РќРµРїСЂР°РІРёР»СЊРЅС‹Р№ Р»РѕРіРёРЅ РёР»Рё РїР°СЂРѕР»СЊ.\n")
             return False
 
     def display_products(self):
         self.store.cursor.execute('SELECT * FROM products')
         products = self.store.cursor.fetchall()
-        print("Товары в магазине.\n")
+        print("РўРѕРІР°СЂС‹ РІ РјР°РіР°Р·РёРЅРµ.\n")
         for product in products:
             print(f"{product[0]}. {product[1]} - ${product[2]}")
         return products
 
     def add_to_cart(self, product_id):
-        self.store.cursor.execute('INSERT INTO shopping_cart (user_id, product_id) VALUES (?, ?)',
+        self.store.cursor.execute('INSERT INTO basket (user_id, product_id) VALUES (?, ?)',
                                   (self.authenticated_user[0], product_id))
         self.store.conn.commit()
 
@@ -48,12 +48,12 @@ class Client(User):
             product_id = int(product_id)
             if not (1 <= product_id <= len(products)):
                 print(
-                    "Неверный номер товара.\n")
+                    "РќРµРІРµСЂРЅС‹Р№ РЅРѕРјРµСЂ С‚РѕРІР°СЂР°.\n")
                 return False
             return True
         except ValueError:
             print(
-                "Неверный ввод.\n")
+                "РќРµРІРµСЂРЅС‹Р№ РІРІРѕРґ.\n")
             return False
 
     def add_to_cart(self, product_id):
@@ -61,34 +61,34 @@ class Client(User):
         if not self.validate_product_id(product_id, products):
             return
 
-        self.store.cursor.execute('INSERT INTO shopping_cart (user_id, product_id) VALUES (?, ?)',
+        self.store.cursor.execute('INSERT INTO basket (user_id, product_id) VALUES (?, ?)',
                                   (self.authenticated_user[0], product_id))
         self.store.conn.commit()
 
     def display_cart(self):
         self.store.cursor.execute('''
             SELECT products.name, products.price
-            FROM shopping_cart
-            JOIN products ON shopping_cart.product_id = products.id
-            WHERE shopping_cart.user_id = ?
+            FROM basket
+            JOIN products ON basket.product_id = products.id
+            WHERE basket.user_id = ?
         ''', (self.authenticated_user[0],))
         cart_items = self.store.cursor.fetchall()
 
         if cart_items:
             total_sum = sum(item[1] for item in cart_items)
-            print("Товары в вашей корзине.\n")
+            print("РўРѕРІР°СЂС‹ РІ РІР°С€РµР№ РєРѕСЂР·РёРЅРµ.\n")
             for item in cart_items:
                 print(f"{item[0]} - ${item[1]}")
-            print(f"Итого: ${total_sum}")
+            print(f"РС‚РѕРіРѕ: ${total_sum}")
         else:
-            print("Ваша корзина пуста.\n")
+            print("Р’Р°С€Р° РєРѕСЂР·РёРЅР° РїСѓСЃС‚Р°.\n")
 
-    def checkout(self, address):
+    def checkout(self):
         self.store.cursor.execute('''
             SELECT products.price
-            FROM shopping_cart
-            JOIN products ON shopping_cart.product_id = products.id
-            WHERE shopping_cart.user_id = ?
+            FROM basket
+            JOIN products ON basket.product_id = products.id
+            WHERE basket.user_id = ?
         ''', (self.authenticated_user[0],))
         prices = self.store.cursor.fetchall()
         total_sum = sum(price[0] for price in prices)
@@ -96,31 +96,31 @@ class Client(User):
         order_number = random.randint(1000, 9999)
 
         self.store.cursor.execute('''
-            INSERT INTO orders (user_id, address, amount, order_number)
+            INSERT INTO orders (user_id, summ, order_number)
             VALUES (?, ?, ?, ?)
-        ''', (self.authenticated_user[0], address, total_sum, order_number))
+        ''', (self.authenticated_user[0], total_sum, order_number))
         self.store.conn.commit()
 
-        self.store.cursor.execute('DELETE FROM shopping_cart WHERE user_id = ?', (self.authenticated_user[0],))
+        self.store.cursor.execute('DELETE FROM basket WHERE user_id = ?', (self.authenticated_user[0],))
         self.store.conn.commit()
 
-        print("Заказ оформлен\n")
-        print(f"Ваш номер заказа №{order_number}")
-        print(f"Итого: ${total_sum}")
+        print("Р—Р°РєР°Р· РѕС„РѕСЂРјР»РµРЅ\n")
+        print(f"Р’Р°С€ РЅРѕРјРµСЂ Р·Р°РєР°Р·Р° в„–{order_number}")
+        print(f"РС‚РѕРіРѕ: ${total_sum}")
 
 class Administrator(User):
-    ADMIN_USERNAME = "admini"
-    ADMIN_PASSWORD = "123456789"
+    admin_name = "admini"
+    admin_password = "12345"
 
     def authenticate(self, login, password):
-        return login == self.ADMIN_USERNAME and password == self.ADMIN_PASSWORD
+        return login == self.admin_name and password == self.admin_password
 
     def add_product(self, name, price):
         self.store.cursor.execute('INSERT INTO products (name, price) VALUES (?, ?)', (name, price))
         self.store.conn.commit()
 
     def remove_product(self, name):
-        self.store.cursor.execute('DELETE FROM products WHERE name = ?', (name,))
+        self.store.cursor.execute('DELETE FROM products WHERE name = ?', (name))
         self.store.conn.commit()
 
     def change_product_name(self, current_name, new_name):
